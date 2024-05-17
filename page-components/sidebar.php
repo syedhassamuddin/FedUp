b44a2bcb4c6c95282ae0e7340b1034e7f0ac30ce
Syed Muhammad Hassam Uddin <smhassamuddin.pk@gmail.com>
2024-05-17 11:50:12 +0500

    Cleaned SideNav Bar

--- page-components/sidebar.php
+++ page-components/sidebar.php
@@ -16,18 +16,18 @@
             			</a>
 					</li>
 
-					<li class="sidebar-item <?php if($activePage == 'packages'){echo'active';}?>">
-						<a class="sidebar-link" href="packages.php">
-              				<i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Packages</span>
-            			</a>
-					</li>
-
 					<li class="sidebar-item <?php if($activePage == 'sendpackage'){echo'active';}?>">
 						<a class="sidebar-link" href="sendpackage.php">
-              				<i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Send Package</span>
+							<i class="align-middle" data-feather="truck"></i> <span class="align-middle">Send Package</span>
             			</a>
 					</li>
 
+					<li class="sidebar-item <?php if($activePage == 'packages'){echo'active';}?>">
+						<a class="sidebar-link" href="packages.php">
+							  <i class="align-middle" data-feather="layers"></i> <span class="align-middle">Packages</span>
+						</a>
+					</li>
+					
 					<li class="sidebar-item <?php if($activePage == 'create-account'){echo'active';}?>">
 						<a class="sidebar-link" href="create-account.php">
 							<i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Create Account</span>
@@ -36,7 +36,7 @@
 
 					<li class="sidebar-item <?php if($activePage == 'view-accounts'){echo'active';}?>">
 						<a class="sidebar-link" href="view-accounts.php">
-              				<i class="align-middle" data-feather="sliders"></i> <span class="align-middle">View Accounts</span>
+              				<i class="align-middle" data-feather="list"></i> <span class="align-middle">View Accounts</span>
             			</a>
 					</li>
 
@@ -46,53 +46,9 @@
             			</a>
 					</li>
 
-					<li class="sidebar-header">
-						Tools & Components
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-buttons.php">
-              				<i class="align-middle" data-feather="square"></i> <span class="align-middle">Buttons</span>
-            			</a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-forms.php">
-              				<i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Forms</span>
-            			</a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-cards.php">
-              				<i class="align-middle" data-feather="grid"></i> <span class="align-middle">Cards</span>
-            			</a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-typography.php">
-              				<i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Typography</span>
-            			</a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="icons-feather.php">
-              				<i class="align-middle" data-feather="coffee"></i> <span class="align-middle">Icons</span>
-            			</a>
-					</li>
-
-					<li class="sidebar-header">
-						Plugins & Addons
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="charts-chartjs.php">
-              				<i class="align-middle" data-feather="bar-chart-2"></i> <span class="align-middle">Charts</span>
-            			</a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="maps-google.php">
-              				<i class="align-middle" data-feather="map"></i> <span class="align-middle">Maps</span>
+					<li class="sidebar-item <?php if($activePage == 'reports'){echo'active';}?>">
+						<a class="sidebar-link" href="reports.php">
+              				<i class="align-middle" data-feather="bar-chart"></i> <span class="align-middle">Reports</span>
             			</a>
 					</li>
 				</ul>
