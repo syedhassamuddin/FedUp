ac74f7bff69e0b0e2feab4e018102e129496fe50
Syed Muhammad Hassam Uddin <smhassamuddin.pk@gmail.com>
2024-05-24 22:15:41 +0500

    Corrected Entire Website's CSS

--- sendpackage.php
+++ sendpackage.php
@@ -23,13 +23,13 @@
 	<meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
 
 	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link rel="shortcut icon" href="img/icons/icon-48x48.png" />
+	<link rel="shortcut icon" href="assets/img/icons/icon-48x48.png" />
 
 	<link rel="canonical" href="https://demo-basic.adminkit.io/" />
 
 	<title>Fed Up Admin</title>
 
-	<link href="css/app.css" rel="stylesheet">
+	<link href="assets/css/app.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
 </head>
 
@@ -39,13 +39,13 @@
 
 	<?php
 		$activePage = "sendpackage";
-		include "page-components/sidebar.php";	
+		include "assets/page-components/sidebar.php";	
 	?>
 
 		<div class="main">
 
 		<?php
-			include "page-components/topnavbar.php";	
+			include "assets/page-components/topnavbar.php";	
 		?>
 			
 			<?php
@@ -234,7 +234,7 @@
 		</div>
 	</div>
 
-	<script src="js/app.js"></script>
+	<script src="assets/js/app.js"></script>
 
 	<script>
 		document.addEventListener("DOMContentLoaded", function() {
