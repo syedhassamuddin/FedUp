a93219bf8138a7dec956d431dd0e008af9a29d5b
Syed Muhammad Hassam Uddin <smhassamuddin.pk@gmail.com>
2024-05-26 17:17:10 +0500

    Help from Hammad

--- assets/page-components/footer.php
+++ assets/page-components/footer.php
@@ -1,27 +1,27 @@
 <footer class="footer">
-				<div class="container-fluid">
-					<div class="row text-muted">
-						<div class="col-6 text-start">
-							<p class="mb-0">
-								<strong>FedUp</strong></a> - <strong>Because why not meri jan</strong>
-							</p>
-						</div>
-						<div class="col-6 text-end">
-							<ul class="list-inline">
-								<li class="list-inline-item">
-									<a class="text-muted" href="#" target="_blank">Support</a>
-								</li>
-								<li class="list-inline-item">
-									<a class="text-muted" href="#" target="_blank">Help Center</a>
-								</li>
-								<li class="list-inline-item">
-									<a class="text-muted" href="#" target="_blank">Privacy</a>
-								</li>
-								<li class="list-inline-item">
-									<a class="text-muted" href="#" target="_blank">Terms</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</footer>
+	<div class="container-fluid">
+		<div class="row text-muted">
+			<div class="col-6 text-start">
+				<p class="mb-0">
+					<strong>FedUp</strong></a> - <strong>Because why not meri jan</strong>
+				</p>
+			</div>
+			<div class="col-6 text-end">
+				<ul class="list-inline">
+					<li class="list-inline-item">
+						<a class="text-muted" href="#" target="_blank">Support</a>
+					</li>
+					<li class="list-inline-item">
+						<a class="text-muted" href="#" target="_blank">Help Center</a>
+					</li>
+					<li class="list-inline-item">
+						<a class="text-muted" href="#" target="_blank">Privacy</a>
+					</li>
+					<li class="list-inline-item">
+						<a class="text-muted" href="#" target="_blank">Terms</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
